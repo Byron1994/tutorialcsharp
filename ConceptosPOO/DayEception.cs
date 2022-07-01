0b03fe423b936776f4c6dfc3a30dec7a3f7a7098
byron <byronramirez1994@gmail.com>
2022-06-30 22:44:59 -0500

    Creacion de clase Date

---
+++ ConceptosPOO/DayEception.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConceptosPOO
+{
+    [Serializable]
+    internal class DayEception : Exception
+    {
+        public DayEception()
+        {
+        }
+
+        public DayEception(string? message) : base(message)
+        {
+        }
+
+        public DayEception(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected DayEception(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ ConceptosPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptosPOO;
+Console.WriteLine("*********MI APLICACION********");
+
+try
+{
+    Console.WriteLine(new Date(2022, 11, 33));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
+
+
+
